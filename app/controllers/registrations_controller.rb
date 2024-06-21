a87036ab2534f7fe90c885a687bc25e8ac6ba2a4
RayNawara <rjnawara@gmail.com>
2024-06-21 13:28:26 -0400

    WIP

--- app/controllers/registrations_controller.rb
+++
@@ -1,9 +0,0 @@
-# app/controllers/admin/registrations_controller.rb
-class Admin::RegistrationsController < Devise::RegistrationsController
-  def update
-    if account_update_params[:admin] == true
-      redirect_to root_path, alert: "Only admins can update the admin role." and return unless current_user.admin?
-    end
-    super
-  end
-end
