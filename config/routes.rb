f3bc5900e33719f39898f1b4d244f060c812890c
RayNawara <rjnawara@gmail.com>
2024-06-21 13:55:59 -0400

    Added /up to routes

--- config/routes.rb
+++ config/routes.rb
@@ -27,4 +27,8 @@ Rails.application.routes.draw do
   post 'accept_follow', to: 'follows#accept_follow', as: :accept_follow
   delete 'decline_follow', to: 'follows#decline_follow', as: :decline_follow
 
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
 end
